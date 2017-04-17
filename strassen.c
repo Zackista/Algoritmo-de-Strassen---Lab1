39c355876f0e82d4f00c28a11838f412a30ebb57
Zackista <cop.olivares@gmail.com>
2017-04-17 18:25:40 -0300

    Algoritmo terminado, listo para hacer experimentos, falta modificar "estilo"

--- strassen.c
+++ strassen.c
@@ -1,19 +1,18 @@
-#include <time.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <math.h>
 
 int n, n0;
 
-int padding(int n);
-void mostrar (double** m, int n);
-void mult(double **m1, double **m2, double **m3, int n);
-void strassen(double **m1, double **m2, double **m3, int dim);
+void mostrar(double **m, int n);
+void multi_clasica(double **m1, double **m2, double **m3, int n);
+void strassen(double** m1, double** m2, double**m3, int dim);
+int padding(int n);	
 
 
-
-int main (int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+	int i, j, aux, modo;
 	FILE *file;
 	srand(time(NULL));
 	if (argc != 3)
@@ -21,404 +20,277 @@ int main (int argc, char *argv[])
 		printf("Ingrese solo tipo de multiplicación, n y n0 \n");
 		end(0);
 	}
-	int opcion, i, j, j1, aux;
-	double **m1, **m2, **m3;
 	float tiempo = 0;
-	clock_t inicio1, fin1; // Se crea variable 'clock_t' para calcular el tiempo de ejecúción del programa.
-	inicio1 = clock();	
-	n = atoi(argv[1]);
-	n0 = atoi(argv[2]);
+ 	clock_t inicio1, fin1; // Se crea variable 'clock_t' para calcular el tiempo de ejecúción del programa.
+	inicio1 = clock();		
+
+ 	
+	n=atoi(argv[1]);		//tamaño de matriz
+	n0=atoi(argv[2]);		// Punto en el cual se deja de usar la técnica de strassen o divide y conquistm1, y se comienza a utilizar la multiplicación clásica
 	aux = n;
 
+	if(n <= 0)
+
+		return 0;
 	n0 = padding(n0);
-	n = padding(n);
+	n = padding(n);//Para asegurarnos de tener una matriz cuadrada.
 	
-
-	m1 = calloc(n,sizeof(double*));
-	m2 = calloc(n,sizeof(double*));
+	double**m1, **m2, **m3;
+	m1 = calloc(n,sizeof(double*));		
+	m2 = calloc(n,sizeof(double*));	
 	m3 = calloc(n,sizeof(double*));
-
-	for (i = 0; i < n; i = i + 1)
-	{
-		m3[i] = calloc(n,sizeof(double*));
-		m1[i] = calloc(n,sizeof(double*));
-		m2[i] = calloc(n,sizeof(double*)); 
+	for(i=0;i<n;i++){
+		m3[i] = calloc(n,sizeof(double));
+		m1[i] = calloc(n,sizeof(double));		
+		m2[i] = calloc(n,sizeof(double));	
+		
 	}
-
- 	for (i = 0; i < aux; i = i + 1)
-	{
-		for (j = 0; j < aux; j = j + 1)
-		{
-			int r = rand() % 100;
-			m1[i][j] = r;
+	for(i=0;i<aux;i++){		// se generan números aleatorios entre 0 y sqrt(tamañomatriz)
+		for(j=0;j<aux;j++){
+			m1[i][j]=((int)rand())%(100);
 		}
 	}
- 	for (i = 0; i < aux; i = i + 1)
-	{
-		for (j = 0; j < aux; j = j + 1)
-		{
-			int r = rand() % 100;
-			m2[i][j] = r;
+	for(i=0;i<aux;i++){
+		for(j=0;j<aux;j++){
+			m2[i][j]=((int)rand())%(100);
 		}
-	}	
+	}
 
-	//en caso de que no sea cuadrada
-
-	for (i = aux; i < n; i = i + 1)
-	{
-		for(j = 0; j < n; j = j + 1)
-		{
+	for(i = aux; i < n; i++)//En caso de que la matriz no sea cuadrada se rellena el resto de la matriz con ceros
+		for(j = 0; j < n; j++)
 			m1[i][j] = 0;
-		}
-	}
-		for (i = aux; i < n; i = i + 1)
-	{
-		for(j = 0; j < n; j = j + 1)
-		{
-			m2[i][j] = 0;
-		}
-	}
 
-	mult(m1,m2,m3,aux);
- 	fin1=clock();
+	for(i = aux; i < n; i++)
+		for(j = 0; j < n; j++)
+			m2[i][j] = 0;
+	
+	multi_clasica(m1,m2,m3,aux);
+	// mostrar(m1,aux);		//se muestran matrices generadas y el resultado de la multiplicación de estas.
+	// mostrar(m2,aux);
+	// mostrar(m3,aux);
+
+	fin1=clock();
 	tiempo = (fin1-inicio1)/(double)CLOCKS_PER_SEC;
 	printf("El tiempo de ejecución fue de: %f \n",tiempo); // %f es porque es un flotante!!!
  	file = fopen("expstrassen.txt", "a");
- 	fprintf(file, "Multiplicacion clasica N: %d, n0: %d, tiempo: %F  \t ", aux, n0, tiempo);
+ 	fprintf(file, "Multiplicacion clasica N: %d, n0: %d, tiempo: %F  \t ", n, n0, tiempo);
 
- 	printf("Matriz 1 clasica \n");
- 	mostrar (m1, aux);
- 	printf("\n");
- 	printf("Matriz 2 \n");
- 	mostrar (m2, aux);
- 	printf("\n\n\n");
- 	printf("Matriz resultado \n");
- 	mostrar (m3, aux);
-
-
-	for (i = 0; i < n; i = i + 1)
-	{
-		for (j = 0; j < n; j = j + 1)
-		{
-			m3[i][j] = 0;
-		}
-	}
 
  	clock_t inicio2, fin2;
  	inicio2 = clock();
- 
+
+	strassen(m1,m2,m3,n);		
+	// mostrar(m1,aux);		//se muestran matrices generadas y el resultado de la multiplicación de estas.
+	// mostrar(m2,aux);
+	// mostrar(m3,aux);
+
+	fin2=clock();
+	tiempo = (fin2-inicio2)/(double)CLOCKS_PER_SEC;
+	printf("El tiempo de ejecución fue de: %f \n",tiempo); // %f es porque es un flotante!!!
+	fprintf(file, "Multiplicacion Strassen N: %d, n0: %d, tiempo: %F  \n", n, n0, tiempo);
+
+	return 0;
+	
+}
+
+void mostrar(double **m, int n){ 		//Muestra los elementos de la matriz resultados
+	int i,j;
+	for (i=0;i<n;i++){
+		for (j=0;j<n;j++)
+		{ 
+			printf("%lf\t",m[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void multi_clasica(double **m1, double **m2, double **m3, int n){	//realiza la multiplicación clásica con n³
+	int i, j, k;
+	for(i=0; i<n; ++i){
+    	for(j=0; j<n; ++j){
+			m3[i][j]=0;
+		    for(k=0; k<n; ++k)
+		    {
+		        m3[i][j]+=m1[i][k]*m2[k][j];
+		    }
+		}
+	}
+	
+}
 
 
-		strassen(m1,m2,m3,n);
-		fin2=clock();
-		tiempo = (fin2-inicio2)/(double)CLOCKS_PER_SEC;
-		printf("El tiempo de ejecución fue de: %f \n",tiempo); // %f es porque es un flotante!!!
-		fprintf(file, "Multiplicacion Strassen N: %d, n0: %d, tiempo: %F  \n", aux, n0, tiempo);
- 	
+void strassen(double** m1, double** m2, double**m3, int dim) {		//Realiza la multiplicación haciendo uso de 7 multiplicaciones y varias sumas
 
 	
- 	printf("Matriz 1 strassen \n");
- 	mostrar (m1, aux);
- 	printf("\n");
- 	printf("Matriz 2 \n");
- 	mostrar (m2, aux);
- 	printf("\n\n\n");
- 	printf("Matriz resultado \n");
- 	mostrar (m3, aux);
+	double **p1 , **p2 , **p3 , **p4, **p5 , **p6 , **p7;
+	double **aux1 , **aux2;
+	int i ,j; 
+	if(dim > n0) { 
+		p1 = calloc(dim/2,sizeof(double*));		
+		p2 = calloc(dim/2,sizeof(double*)); 
+		p3 = calloc(dim/2,sizeof(double*));		
+		p4 = calloc(dim/2,sizeof(double*));				//se pide la memoria para las 7 submatrices y 2 matrices que se usaran como auxiliar
+		p5 = calloc(dim/2,sizeof(double*));		
+		p6 = calloc(dim/2,sizeof(double*));		
+		p7 = calloc(dim/2,sizeof(double*));		
+		
+		aux1 = calloc(dim/2,sizeof(double*));		
+		aux2 = calloc(dim/2,sizeof(double*));	
+		for(i=0;i<dim/2;i++){
+			p1[i] = calloc(dim/2,sizeof(double));
+			p2[i] = calloc(dim/2,sizeof(double));
+			p3[i] = calloc(dim/2,sizeof(double));
+			p4[i] = calloc(dim/2,sizeof(double));
+			p5[i] = calloc(dim/2,sizeof(double));
+			p6[i] = calloc(dim/2,sizeof(double));
+			p7[i] = calloc(dim/2,sizeof(double));
+			aux1[i] = calloc(dim/2,sizeof(double));		
+			aux2[i] = calloc(dim/2,sizeof(double));	
+		}
+		//p1								SE REALIZAN LAS 7 MULTIPLICACIONES GENERANDO SUB MATRICES
 
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux1[i][j] = m1[i][j] + m1[i + dim / 2][j + dim / 2];
+		}
+		
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux2[i][j] = m2[i][j] + m2[i + dim / 2][j + dim / 2];
+		}
+		n = dim / 2;
+		strassen(aux1, aux2, p1, dim / 2);
 
+		//p2
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux1[i][j] = m1[i + dim / 2][j] + m1[i + dim / 2][j + dim / 2];
+		}
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux2[i][j] = m2[i][j];
+		}
+		n = dim / 2;
+		strassen(aux1, aux2, p2, dim / 2);
 
-    if(file == NULL)
-    {
-        printf("Error al abrir archivo");
-    }
-    // for (i = 0; i < n; i = i + 1)
-    // {
-    // 	if (i != 0)
-    // 	{
-    // 		fprintf(file, "\n");
-    // 	}
-    // 	for (j = 0; j < n; j = j + 1)
-    // 	{
-    // 		fprintf(file,"%lf \t \t %lf ",m1[i][j]), m2[i][j];
-    // 		printf("hola");
-    // 	}
-    // }
+		//p3
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux1[i][j] = m1[i][j];
+		}
 
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux2[i][j] = m2[i][j + dim / 2] - m2[i + dim / 2][j + dim / 2];	
+		}
+		n = dim / 2;
+		strassen(aux1, aux2, p3, dim / 2);
 
-    // fprintf(file,"\t \t ");
+		//p4
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux1[i][j] = m1[i + dim / 2][j + dim / 2];
+		}
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux2[i][j] = m2[i + dim / 2][j] - m2[i][j];
+		}
+		n = dim / 2;
+		strassen(aux1, aux2, p4, dim / 2);
 
-  //   for (i = 0; i < aux; i = i + 1)
-  //   {
+		//p5
 
-		// fprintf(file, "\n");    	
-		// for (j = 0; j < aux; j = j + 1)
-		// {
-  //   		fprintf(file,"%lf ",m1[i][j]);
-  //   		if (j == aux-1)
-  //   		{
-  //   			fprintf(file, "\t\t");   
-  //   			for(j1 = 0; j1 < aux; j1 = j1 + 1)
-  //   			{
-	 //    			fprintf(file, "%lf ",m2[i][j1] );
-	 //    			if (j1 == aux-1 && i == 0)
-	 //    			{
-		// 				fprintf(file, "\t \t %F ",tiempo);
-	 //    			}
-	 //    			if (j1 == aux-1 && i == aux-1)
-	 //    			{
-	 //    				fprintf(file, "\n\n\n"); 
-	 //    			}
-	 //    		}
-  //   		}
-		// }
-  //   }
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux1[i][j] = m1[i][j] + m1[i][j + dim / 2];
+		}
 
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux2[i][j] = m2[i + dim / 2][j + dim / 2];
+		}
+		n = dim / 2;
+		strassen(aux1, aux2, p5, dim / 2);
 
-    return 0;
+		//p6
 
-    // fprintf(file, "\t \t %F \n", tiempo);
-}
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux1[i][j] = m1[i + dim / 2][j] - m1[i][j];
+		}
+		n = dim / 2;
 
-void mult(double **m1, double ** m2, double **m3, int n)
-{
- 	int i, j, k;
-	for (i = 0; i < n; i = i + 1)
-	{
-		for (j = 0; j < n; j = j + 1)
-		{
-			m3[i][j] = 0;
-			for (k = 0; k < n; k = k + 1)
-			{
-				m3[i][j] = m3[i][j] + m1[i][k] * m2[k][j];
-			}
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux2[i][j] = m2[i][j] + m2[i][j + dim / 2];
+		}
+
+		n = dim / 2;
+		strassen(aux1, aux2, p6, dim / 2);
+
+		//p7
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux1[i][j] = m1[i][j + dim / 2] - m1[i + dim / 2][j + dim / 2];
+		}
+
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				aux2[i][j] = m2[i + dim / 2][j] + m2[i + dim / 2][j + dim / 2];
+		}
+		n = dim / 2;
+		strassen(aux1, aux2, p7, dim / 2);
+		
+		//c11
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				m3[i][j] = p1[i][j] + p4[i][j] - p5[i][j] + p7[i][j];
+		}
+
+		//c12
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				m3[i][j + dim / 2] = p3[i][j] + p5[i][j];
+		}
+		//c21
+		for(i = 0; i < dim / 2; i++) {
+
+			for(j = 0; j < dim / 2; j++) 
+				m3[i + dim / 2][j] = p2[i][j] + p4[i][j];
+		}
+		
+		//c22
+		for(i = 0; i < dim / 2; i++) {
+			for(j = 0; j < dim / 2; j++) 
+				m3[i + dim / 2][j + dim / 2] = p1[i][j] + p3[i][j] - p2[i][j] + p6[i][j];
+		}
+	}else{
+		if(dim==n0){	//Si hemos llegado a n0 se usa multiplicación clásica
+			multi_clasica(m1,m2,m3,dim);
 		}
 	}
+
 }
 
-void strassen(double **m1, double ** m2, double **m3, int dim)
-{
-
-	double **p1, **p2, **p3, **p4, **p5, **p6, **p7;
-	double **aux, **aux2;
-	int i ,j;
-	if (dim % 2 == 0)
-	{
-		if(dim > n0) 
-		{ 
-			p1 = calloc(dim/2,sizeof(double*));		
-			p2 = calloc(dim/2,sizeof(double*)); 
-			p3 = calloc(dim/2,sizeof(double*));		
-			p4 = calloc(dim/2,sizeof(double*));				//se pide la memoria para las 7 submatrices y 2 matrices que se usaran como auxiliar
-			p5 = calloc(dim/2,sizeof(double*));		
-			p6 = calloc(dim/2,sizeof(double*));		
-			p7 = calloc(dim/2,sizeof(double*));		
-			
-			aux = calloc(dim/2,sizeof(double*));		
-			aux2 = calloc(dim/2,sizeof(double*));	
-			for(i = 0; i < dim/2; i = i + 1)
-			{
-				p1[i] = calloc(dim/2,sizeof(double*));
-				p2[i] = calloc(dim/2,sizeof(double*));
-				p3[i] = calloc(dim/2,sizeof(double*));
-				p4[i] = calloc(dim/2,sizeof(double*));
-				p5[i] = calloc(dim/2,sizeof(double*));
-				p6[i] = calloc(dim/2,sizeof(double*));
-				p7[i] = calloc(dim/2,sizeof(double*));
-				aux[i] = calloc(dim/2,sizeof(double*));		
-				aux2[i] = calloc(dim/2,sizeof(double*));	
-			}
-
-			for(i = 0; i < dim / 2; i = i + 1) 
-			{
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux[i][j] = m1[i][j] + m1[i + dim / 2][j + dim / 2];
-			}
-			
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux2[i][j] = m2[i][j] + m2[i + dim / 2][j + dim / 2];
-			}
-			n = dim / 2;
-			strassen(aux, aux2, p1, dim / 2);
-
-			//p2
-			for(i = 0; i < n / 2; i = i + 1) {
-				for(j = 0; j < n / 2; j = j + 1) 
-					aux[i][j] = m1[i + dim / 2][j] + m1[i + dim / 2][j + dim / 2];
-			}
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux2[i][j] = m2[i][j];
-			}
-			n = dim / 2;
-			strassen(aux, aux2, p2, dim / 2);
-
-			//p3
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux[i][j] = m1[i][j];
-			}
-
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux2[i][j] = m2[i][j + dim / 2] - m2[i + dim / 2][j + dim / 2];	
-			}
-			n = dim / 2;
-			strassen(aux, aux2, p3, dim / 2);
-
-			//p4
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux[i][j] = m1[i + dim / 2][j + dim / 2];
-			}
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux2[i][j] = m2[i + dim / 2][j] - m2[i][j];
-			}
-			n = dim / 2;
-			strassen(aux, aux2, p4, dim / 2);
-
-			//p5
-
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux[i][j] = m1[i][j] + m1[i][j + dim / 2];
-			}
-
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux2[i][j] = m2[i + dim / 2][j + dim / 2];
-			}
-			n = dim / 2;
-			strassen(aux, aux2, p5, dim / 2);
-
-			//p6
-
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux[i][j] = m1[i + dim / 2][j] - m1[i][j];
-			}
-			n = dim / 2;
-
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux2[i][j] = m2[i][j] + m2[i][j + dim / 2];
-			}
-
-			n = dim / 2;
-			strassen(aux, aux2, p6, dim / 2);
-
-			//p7
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux[i][j] = m1[i][j + dim / 2] - m1[i + dim / 2][j + dim / 2];
-			}
-
-			for(i = 0; i < dim / 2; i = i + 1)
-			{
-				for(j = 0; j < dim / 2; j = j + 1) 
-					aux2[i][j] = m2[i + dim / 2][j] + m2[i + dim / 2][j + dim / 2];
-			}
-			n = dim / 2;
-			strassen(aux, aux2, p7, dim/ 2);
-			
-			//c11
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					m3[i][j] = p1[i][j] + p4[i][j] - p5[i][j] + p7[i][j];
-			}
-
-			//c12
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					m3[i][j + dim / 2] = p3[i][j] + p5[i][j];
-			}
-			//c21
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					m3[i + dim / 2][j] = p2[i][j] + p4[i][j];
-			}
-			
-			//c22
-			for(i = 0; i < dim / 2; i = i + 1) {
-				for(j = 0; j < dim / 2; j = j + 1) 
-					m3[i + dim / 2][j + dim / 2] = p1[i][j] + p3[i][j] - p2[i][j] + p6[i][j];
-			}
-		}
-		else
-		{
-			if(dim==n0)
-			{	//Si hemos llegado a n0 se usa multiplicación clásica
-				printf("paseeeeeeee 22\n");
-				mult(m1,m2,m3,dim);
-
-			}
-		}
-	}
-	else
-	{
-		printf("paseeeeee\n");
-		mult(m1,m2,m3,dim);
-	}
-}
-
-int padding(int n) 
-{  // Busca la potencia de dos más cercana por arriba del número para segurar una matriz cuadrada
+int padding(int n) {  // Busca la potencia de dos más cercana por arriba del número para segurar una matriz cuadrada
 	int original_n = n, pot_menor = 0, i, act_n = 1;
 
 	if(n == 1)
-	{
 		return 1;
-	}
-	while(n > 1) 
-	{
-		pot_menor = pot_menor + 1;
-		n = n / 2;
+
+	while(n > 1) {
+		pot_menor++;
+		n /= 2;
 	}
 
-	for(i = 0; i < pot_menor; i = i + 1) 
-	{
-		act_n = act_n * 2;
+	for(i = 0; i < pot_menor; i++) {
+		act_n *= 2;
 	}
 
 	if(act_n == original_n)
-	{	
 		return original_n;
-	}
 	else
-	{	
 		return act_n * 2;
-	}
+
 }
-
-void mostrar(double **m, int n)
-{
-	int i, j;
-	for (i = 0; i < n; i = i + 1)
-	{
-		for (j = 0; j < n; j = j + 1)
-		{
-			printf("[");
-			printf("%lf",m[i][j] );
-			printf("]");
-		}
-	printf("\n");
-	}
-}	
-
-
- //  void mostrar(double** m, int n)
- //  {
- //  	int i,j;
- //  	for (i = 0; i < n; i = i + 1)
-	// {
-	// 	for (j = 0; j < n; j = j + 1)
-	// 	{
-	// 		printf("[");
-	// 		printf("%lf",m[i][j] );
-	// 		printf("]");
-	// 	}
-	// 	printf("\n \n");
-	// }
- //  }
